4158a198772706cf9ec94f3b8f96571e592172e0
unionsoftware <ggmortum@hotmail.com>
2015-10-25 21:07:57 -0500

    ultimo

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ echo "Running"
 echo "Add all files"
 git add .
 git commit -m $1
-git remote set-url  origin https://github.com/unionsoftware/nodejsv1.gi
+git remote set-url  origin https://github.com/unionsoftware/nodejsv1
 git push -f origin master
 
